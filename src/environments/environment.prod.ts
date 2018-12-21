fb33809913a620cd798aa882f3aa77f35d6ad34c
anamariaagudelo <anamariaagudeloisi@gmail.com>
2018-12-21 09:31:34 -0500

    hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDEsC0uh5QZgGUAPlP312uySW3OrDXfHIY',
+    authDomain: 'appwebpmo-6d463.firebaseapp.com',
+    databaseURL: 'https://appwebpmo-6d463.firebaseio.com',
+    projectId: 'appwebpmo-6d463',
+    storageBucket: 'appwebpmo-6d463.appspot.com',
+    messagingSenderId: '980531451968'
+  }
 };
